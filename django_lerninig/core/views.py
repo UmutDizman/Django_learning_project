78a45d8fbe6a85cd5dac155143af8001786db8b9
Umut <umutd.zman@gmail.com>
2025-08-22 06:28:47 +0300

    Dinamic routing added

--- django_lerninig/core/views.py
+++ django_lerninig/core/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+course_dictionarry = {
+    "python": "Python Course Page",
+    "java": "Java course page",
+    "css" :   "CSS course Page"
+}
+
 def index(request):
     return HttpResponse("First django project")
+
+def course(request, item):
+    return HttpResponse(course_dictionarry[item])
